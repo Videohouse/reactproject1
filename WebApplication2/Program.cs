58cae25df0acd801d56437d958e25e62f29a5b7b
Carolien Heymans <carolien@boostgraphics.tv>
2022-07-07 17:35:28 +0200

    get selected players

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -6,9 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoContext>(opt => { 
-    opt.UseInMemoryDatabase("TodoList");
-});
 
 builder.Services.AddDbContext<PlayerContext>(opt => {
     opt.UseInMemoryDatabase("PlayerList");
